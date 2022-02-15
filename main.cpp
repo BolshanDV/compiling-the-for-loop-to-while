5a3644fe9b5e2d33a71264dcbc2017b4fbbac768
BolshanDV <92440782+BolshanDV@users.noreply.github.com>
2022-02-15 22:10:04 +0300

    initial project

---
+++ main.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include "fstream"
+#include <vector>
+
+using namespace std;
+
+// TODO создать 3 структуры под 3 типа фором (ClassicFor ... etc)
+// ClassicFor = {
+// string [] conditionals = {"int i = 0, i < 3, i++"}
+// String [] bodyLines = {"some logic"} split by ";";
+//}
+
+// TODO Создать функцию, которая определяет тип for ("ClassicFor.. etc")
+// TODO Проверяешь через switch case for type и вызываешь функцию парсинга (2 функции парсинга)
+// TODO внутри ф. парсинга вызываешь 2 отдельнвых метода (extractConditionals, extractBody (logic for 2 type body))
+// TODO эти функции будут возвращать твои данные и там ты уже в свои структуры добавляешь эти данные
+// TODO 2 массива, которые содержат объекты
+//
+
+string readingFile(){
+    ifstream file("/Users/dmitrij/CLionProjects/compiling_the_for _loop _to _while/text.txt");
+    string str;
+    string content; //хранится прочтенный фаил
+    while (getline(file, str)) {
+        content += str;
+        content.push_back('\n');
+    };
+    return content;
+};
+
+void creatureReadyFile( string text) {
+    ofstream out;
+    out.open("/Users/dmitrij/CLionProjects/compiling_the_for _loop _to _while/resultat.txt", ios::app);
+    out << text << endl;
+};
+
+struct Condition {
+     string initialize;
+     string condition;
+     string increment;
+};
+
+Condition parserCondition (string conditionBlocFor){
+    Condition elementConditionBlock;
+    int j = 3;
+    while ( j != 0) {
+        string strCondition = conditionBlocFor;
+        switch (j) {
+            case 3: elementConditionBlock.initialize = strCondition.substr(0 ,strCondition.find(';'));
+                break;
+            case 2: elementConditionBlock.condition = strCondition.substr(0 ,strCondition.find(';'));
+                break;
+            case 1: elementConditionBlock.increment = strCondition;
+                break;
+        }
+        conditionBlocFor.erase(0, strCondition.find(';') + 1);
+        j--;
+    };
+    return elementConditionBlock;
+};
+string parserBody(string body) {
+    return body.substr(body.find('{') + 1, body.find('}') - body.find('{') - 1 );
+};
+string parser( const string& i ) {
+    string finalText, initialize, condition, increment, bodyBlocFor;
+    string strCondition = i;
+    string conditionBlocFor = strCondition.substr(i.find('(') + 1, i.find(')')-i.find('(') - 1 );
+    parserCondition(conditionBlocFor);
+    parserBody(i);
+    finalText.append("int " + initialize + "\n" + "while(" + condition + " ){" + bodyBlocFor + increment + "\n}\n");
+
+    return finalText;
+};
+
+
+void processingText( string content) {
+    string str;
+    while (content.length() != 0) {
+        str = content;
+        string index;
+
+        int startElement = str.find("for(");
+        int end = str.find(')');
+        int endElement = str.find('}');
+
+        for (int i = 23; i <= str.length(); i++) {
+            if((i > end)  && (content[i] == ';')){
+                index = str[i];
+                break;
+            };
+        }
+        if ((startElement  == -1) || (endElement == -1)){
+            creatureReadyFile(str.substr(0, str.length()));
+            break;
+        }
+
+        creatureReadyFile(str.substr(0, startElement));
+
+        string forelement = parser(str.substr(startElement, endElement  - startElement + 1));
+
+        creatureReadyFile(forelement);
+
+        content.erase(0, endElement + 1);
+    }
+};
+
+int main() {
+    string content = readingFile();
+
+    processingText(content);
+    
+    return 0;
+}
